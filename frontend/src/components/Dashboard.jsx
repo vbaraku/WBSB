a6ebb0f7c02aa5d53460de039e6a83263306ff43
VBaraku <vbaraku@citycollege.sheffield.eu>
2022-10-11 19:49:56 +0200

    Drawer for categories first impl

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Graphs from './Graphs'
+import MiniDrawer from './drawer/MiniDrawer'
 
 export default function Dashboard() {
   return (
     <div style={{display:"flex", marginTop:"100px"}}>
               {/* <QuestionNav></QuestionNav> */}
-        <div style={{width:"15%"}}>we</div>
+        <div style={{width:"15%"}}> <MiniDrawer></MiniDrawer></div>
         <div style={{width:"85%"}}>
             <Graphs></Graphs>
 
--- frontend/src/utils/dictionaries.js
+++ frontend/src/utils/dictionaries.js
@@ -1,5 +1,6 @@
 const albanian_dict = {
   ALL: "Të gjitha",
+
   KOSOVO_REGIONS: [
     "Të gjitha",
     "Gjilan",
@@ -11,6 +12,7 @@ const albanian_dict = {
     "Gjakovë",
     "Mitrovica e Veriut",
   ],
+
   ALBANIA_REGIONS: [
     "Të gjitha",
     "Berat",
@@ -28,6 +30,7 @@ const albanian_dict = {
   ],
 
   REGION_TYPES: ["Të gjitha", "Rural", "Urban"],
+
   NATIONALITIES: [
     "Të gjitha",
     "Serb",
@@ -40,17 +43,84 @@ const albanian_dict = {
     "Rom",
     "Tjetër",
   ],
+
   GENDERS: ["Të gjitha", "Mashkull", "Femër", "Tjetër"],
+
   YEAR_LABEL: "Viti",
+
   REGION_LABEL: "Regjioni",
+
   REGION_TYPE_LABEL: "Vendbanimi",
+
   NATIONALITY_LABEL: "Kombësia",
+
   GENDER_LABEL: "Gjinia",
+
   AGE_LABEL: "Mosha",
 };
 
 const serbian_dict = {};
 
-const english_dict = {};
+const english_dict = {
+  ALL : 'All',
+
+  KOSOVO_REGIONS: [
+    "All",
+    "Gjilan",
+    "Pristina",
+    "Pejë",
+    "Prizren",
+    "Ferizaj",
+    "Mitrovicë",
+    "Gjakovë",
+    "Northern Mitrovica",
+  ],
+
+  ALBANIA_REGIONS: [
+    "Të gjitha",
+    "Berat",
+    "Durrës",
+    "Elbasan",
+    "Fier",
+    "Gjirokastër",
+    "Korçë",
+    "Kukës",
+    "Lezhë",
+    "Dibër",
+    "Shkodër",
+    "Tiranë",
+    "Vlorë",
+  ],
+
+  REGION_TYPES: ["All", "Rural", "Urban"],
+
+  NATIONALITIES: [
+    "All",
+    "Serb",
+    "Albanian",
+    "Montenegrin",
+    "Egyptian",
+    "Ashkali",
+    "Turkish",
+    "Bosniak",
+    "Roma",
+    "Other",
+  ],
+
+  GENDERS: ["All", "Male", "Female", "Other"],
+
+  YEAR_LABEL: "Year",
+
+  REGION_LABEL: "Region",
+
+  REGION_TYPE_LABEL: "Location",
+
+  NATIONALITY_LABEL: "Nationality",
+
+  GENDER_LABEL: "Gender",
+
+  AGE_LABEL: "Age",
+  
+};
 
 export { albanian_dict, serbian_dict, english_dict };
