a6e5a8368d22211371bf449a7c319cf9ca937fd6
VBaraku <vbaraku@citycollege.sheffield.eu>
2023-03-13 21:01:31 +0100

    Starting implementing ordering of questions

--- frontend-berry/src/components/OrderQuestions.jsx
+++ frontend-berry/src/components/OrderQuestions.jsx
@@ -1,27 +1,30 @@
-import { Button, Box } from '@mui/material';
+import * as React from 'react';
+import { Button, Box, FormControl, Select, InputLabel, MenuItem } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import { InfinitySpin } from 'react-loader-spinner';
 
 export default function OrderQuestions() {
     const columns = [
-        { field: 'id', headerName: 'ID', width: 90 },
+        { field: 'id', headerName: 'ID', flex: 1 },
         {
             field: 'name',
-            headerName: 'Pyetja',
-            width: 700,
+            headerName: 'Question',
+            flex: 7,
             editable: true
         },
         {
             field: 'category',
-            headerName: 'Kategoria',
-            width: 450,
+            headerName: 'Category',
+            flex: 4,
             editable: true
         },
         {
             field: 'order',
-            headerName: 'Renditja',
+            headerName: 'Order',
             type: 'number',
             align: 'left',
-            width: 110,
+            headerAlign: 'left',
+            flex: 1,
             editable: true
         }
     ];
@@ -33,6 +36,12 @@ export default function OrderQuestions() {
     ];
 
     const drawerWidth = 240;
+
+    const [category, setCategory] = React.useState('');
+
+    const handleChange = (event) => {
+        setCategory(event.target.value);
+    };
 
     return (
         <Box sx={{ display: 'flex' }}>
@@ -46,17 +55,43 @@ export default function OrderQuestions() {
                     width: { sm: `calc(100% - ${drawerWidth}px)` }
                 }}
             >
-                <Box sx={{ height: 700, width: '100%', backgroundColor: 'white' }}>
-                    <DataGrid
-                        rows={rows}
-                        columns={columns}
-                        pageSize={5}
-                        rowsPerPageOptions={[5]}
-                        disableSelectionOnClick
-                        experimentalFeatures={{ newEditingApi: true }}
-                    />
+                <Box sx={{ mb: 2, backgroundColor: 'white' }}>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={category}
+                            label="Category"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value={10}>Category AAAAAAAAAAA</MenuItem>
+                            <MenuItem value={20}>Category BBBBBBBBBBB</MenuItem>
+                            <MenuItem value={30}>Category CCCCCCCCCCC</MenuItem>
+                        </Select>
+                    </FormControl>
                 </Box>
-                <Button variant="contained">Save</Button>
+                {category && (
+                    <>
+                        <Box sx={{ height: 700, width: '100%', backgroundColor: 'white' }}>
+                            <DataGrid
+                                rows={rows}
+                                columns={columns}
+                                pageSize={5}
+                                rowsPerPageOptions={[5]}
+                                disableSelectionOnClick
+                                experimentalFeatures={{ newEditingApi: true }}
+                            />
+                        </Box>
+                        <Button variant="contained">Save</Button>
+                    </>
+                )}
+                {!category && (
+                    <Box sx={{ height: 700, width: '100%', backgroundColor: 'white', textAlign: 'center' }}>
+                        <h1 style={{ textAlign: 'center', paddingTop: 25 }}>Pick a category</h1>
+                        <InfinitySpin color="blue" />
+                    </Box>
+                )}
             </Box>
         </Box>
     );
